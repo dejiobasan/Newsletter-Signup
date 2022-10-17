a5df027ab7e61346d977983bf069047af084fecc
Deji Obasan <dejiobasan@yahoo.com>
2022-10-17 13:09:25 +0100

    created a sign up page using bootstrap cdn.

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+//jshint esversion: 6
+
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
 const port = 3000;
@@ -8,4 +11,4 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(port, function () {
     console.log("server started at port 3000.");
-})
+});
