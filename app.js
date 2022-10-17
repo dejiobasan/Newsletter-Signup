dc84fe4afb65374d86cb35911283b2e039add603
Deji Obasan <dejiobasan@yahoo.com>
2022-10-17 13:58:32 +0100

    Created a localhost server to get and post information to the user

--- app.js
+++ app.js
@@ -7,7 +7,19 @@ const request = require("request");
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+    var firstName = req.body.fname;
+    var lastName = req.body.lname;
+    var Email = req.body.email;
+    console.log(firstName, lastName, Email);
+});
 
 app.listen(port, function () {
     console.log("server started at port 3000.");
